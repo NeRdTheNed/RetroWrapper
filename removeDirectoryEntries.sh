662cea77e7a531d6bd8eb83ee2a91a02c71bcdbf
Ned Loynd <ned@loynd.com>
2022-11-08 20:07:58 +1100

    Create a MultiMC instance on build
    Not that I can test this works right now, as the authentication servers are down.

--- removeDirectoryEntries.sh
+++ removeDirectoryEntries.sh
@@ -2,11 +2,46 @@
 
 # Rezips all built .jar files, to remove directory entries.
 # This reduces the file size by a small amount.
-for file in ./build/libs/*.jar
+# TODO Re-use output in other steps
+for file in ./build/libs/*.jar ./build/libs/*.zip
 do
   unzip "$file" -d ./build/libs/temp
   rm "$file"
+  shopt -s nullglob
+  for jarJar in ./build/libs/temp/libraries/*.jar
+  do
+    unzip "$jarJar" -d ./build/libs/temp/libraries/temp
+    rm "$jarJar"
+    advzip "$jarJar" --shrink-store --pedantic -a ./build/libs/temp/libraries/temp/**
+    rm -rf ./build/libs/temp/libraries/temp/
+    strip-nondeterminism "$jarJar"
+    #advzip --shrink-insane -kzi 9 -p "$jarJar"
+    advzip --shrink-extra -kzp "$jarJar"
+  done
+  echo "test $file"
+  for jsonFile in ./build/libs/**/**/**.json ./build/libs/**/**.json ./build/libs/**.json
+  do
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      if ! command -v gsed &> /dev/null
+      then
+        echo "Please install GNU sed as gsed"
+      else
+        echo "json $jsonFile"
+        jq -c . < "$jsonFile" | gsed -z '$ s/\n$//' > "$jsonFile-tempOut"
+      fi
+    else
+      jq -c . < "$jsonFile" | sed -z '$ s/\n$//' > "$jsonFile-tempOut"
+    fi
+    mv "$jsonFile-tempOut" "$jsonFile"
+  done
   # TODO replace this with standard zip
   advzip "$file" --shrink-store --pedantic -a ./build/libs/temp/**
   rm -rf ./build/libs/temp/
+done
+
+for file in ./build/libs/*.jar ./build/libs/*.zip
+do
+  strip-nondeterminism "$file"
+  #advzip --shrink-insane -kzi 09 -p "$file"
+  advzip --shrink-extra -kzp "$file"
 done

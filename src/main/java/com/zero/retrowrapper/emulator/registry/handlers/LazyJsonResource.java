81bbddd6f6c8d42c9b789db2c696765a573fe7d8
Ned Loynd <ned@loynd.com>
2022-12-27 18:46:19 -0500

    Save memory & lazy load sound data

---
+++ src/main/java/com/zero/retrowrapper/emulator/registry/handlers/LazyJsonResource.java
@@ -0,0 +1,100 @@
+package com.zero.retrowrapper.emulator.registry.handlers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.net.ssl.SSLHandshakeException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.commons.lang3.concurrent.ConcurrentException;
+import org.apache.commons.lang3.concurrent.LazyInitializer;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+import com.zero.retrowrapper.emulator.RetroEmulator;
+import com.zero.retrowrapper.util.FileUtil;
+
+import net.minecraft.launchwrapper.Launch;
+import net.minecraft.launchwrapper.LogWrapper;
+
+class LazyJsonResource extends LazyInitializer<JsonObject> {
+    private final String indexName;
+    private final String indexURL;
+
+    LazyJsonResource(final String indexName, final String indexURL) {
+        this.indexName = indexName;
+        this.indexURL = indexURL;
+    }
+
+    @Override
+    protected JsonObject initialize() throws ConcurrentException {
+        final JsonObject tempObjects = downloadSoundData(indexName, indexURL);
+        return tempObjects != null ? tempObjects : Json.object();
+    }
+
+    private static JsonObject downloadSoundData(String indexName, String indexURL) {
+        final File jsonCached = new File(RetroEmulator.getInstance().getCacheDirectory() + File.separator + indexName, indexName + ".json");
+        final File localLegacyJson = FileUtil.tryFindFirstFile(
+                                         jsonCached,
+                                         new File(Launch.minecraftHome + File.separator + "assets" + File.separator + "indexes" + File.separator + indexName + ".json"),
+                                         new File(FileUtil.defaultMinecraftDirectory() + File.separator + "assets" + File.separator + "indexes" + File.separator + indexName + ".json")
+                                     );
+
+        if (localLegacyJson != null) {
+            FileInputStream fis = null;
+
+            try {
+                fis = new FileInputStream(localLegacyJson);
+                final String jsonString = IOUtils.toString(fis);
+                final JsonValue json = Json.parse(jsonString);
+                final JsonObject obj = json.asObject();
+                final JsonObject resourceObjects = obj.get("objects").asObject();
+
+                if (!jsonCached.isFile()) {
+                    FileUtils.copyFile(localLegacyJson, jsonCached);
+                }
+
+                LogWrapper.info("Using local " + indexName + ".json.");
+                return resourceObjects;
+            } catch (final Exception e) {
+                LogWrapper.warning("Exception loading local " + indexName + ".json: " + ExceptionUtils.getStackTrace(e));
+            } finally {
+                IOUtils.closeQuietly(fis);
+            }
+        } else {
+            LogWrapper.warning("Could not find local " + indexName + ".json.");
+        }
+
+        InputStream is = null;
+
+        try {
+            is = new URL(indexURL).openStream();
+            final String jsonString = IOUtils.toString(is);
+            final JsonValue json = Json.parse(jsonString);
+            final JsonObject obj = json.asObject();
+            final JsonObject resourceObjects = obj.get("objects").asObject();
+            // Cache file locally
+            FileUtils.writeStringToFile(jsonCached, jsonString);
+            LogWrapper.info("Using downloaded " + indexName + ".json.");
+            return resourceObjects;
+        } catch (final Exception e) {
+            LogWrapper.warning("Exception downloading " + indexName + ".json: " + ExceptionUtils.getStackTrace(e) + "\nThe sound fix probably won't work.");
+
+            if (e instanceof SSLHandshakeException) {
+                LogWrapper.warning("The Java installation that Minecraft is running on " +
+                                   "(" + SystemUtils.JAVA_VERSION + " (" + SystemUtils.JAVA_VENDOR + " " + SystemUtils.JAVA_VM_VERSION + ") located at " + SystemUtils.JAVA_HOME + ")" +
+                                   " may not support modern versions of TLS/SSL (e.g. TLSv1.3). Consider using a newer Java installation to fix this.");
+            }
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
+
+        return null;
+    }
+}
--- src/main/java/com/zero/retrowrapper/emulator/registry/handlers/ResourcesHandler.java
+++ src/main/java/com/zero/retrowrapper/emulator/registry/handlers/ResourcesHandler.java
@@ -13,15 +13,12 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-import javax.net.ssl.SSLHandshakeException;
-
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.SystemUtils;
+import org.apache.commons.lang3.concurrent.ConcurrentException;
+import org.apache.commons.lang3.concurrent.ConcurrentInitializer;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
-import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 import com.zero.retrowrapper.emulator.RetroEmulator;
@@ -69,8 +66,7 @@ public final class ResourcesHandler extends EmulatorHandler {
     private static final int smallestSize = 16;
     private static final Pattern slashPattern = Pattern.compile("/");
 
-    private final JsonObject jsonObjects;
-    private final byte[] soundsList;
+    private final ConcurrentInitializer<JsonObject> jsonObjects;
     private final String indexName;
     private final ResourcesFormat resourcesFormat;
 
@@ -86,93 +82,42 @@ public final class ResourcesHandler extends EmulatorHandler {
         super(handle);
         this.resourcesFormat = resourcesFormat;
         this.indexName = indexName;
-        final JsonObject tempObjects = downloadSoundData(indexName, indexURL);
-        jsonObjects = tempObjects != null ? tempObjects : Json.object();
-
-        switch (resourcesFormat) {
-        case CLASSIC:
-            soundsList = jsonResourcesToClassic(jsonObjects).getBytes();
-            break;
-
-        case AWS:
-            soundsList = jsonResourcesToAwsXml(jsonObjects).getBytes();
-            break;
-
-        default:
-            LogWrapper.severe("According to all known laws of Javiation, there is no way that a switch should be able to reach this case.");
-            soundsList = null;
-            break;
-        }
-
+        jsonObjects = new LazyJsonResource(indexName, indexURL);
         resourcesPattern = Pattern.compile(handle, Pattern.LITERAL);
     }
 
-    private static JsonObject downloadSoundData(String indexName, String indexURL) {
-        final File jsonCached = new File(RetroEmulator.getInstance().getCacheDirectory() + File.separator + indexName, indexName + ".json");
-        final File localLegacyJson = FileUtil.tryFindFirstFile(
-                                         jsonCached,
-                                         new File(Launch.minecraftHome + File.separator + "assets" + File.separator + "indexes" + File.separator + indexName + ".json"),
-                                         new File(FileUtil.defaultMinecraftDirectory() + File.separator + "assets" + File.separator + "indexes" + File.separator + indexName + ".json")
-                                     );
-
-        if (localLegacyJson != null) {
-            FileInputStream fis = null;
-
-            try {
-                fis = new FileInputStream(localLegacyJson);
-                final String jsonString = IOUtils.toString(fis);
-                final JsonValue json = Json.parse(jsonString);
-                final JsonObject obj = json.asObject();
-                final JsonObject resourceObjects = obj.get("objects").asObject();
-
-                if (!jsonCached.isFile()) {
-                    FileUtils.copyFile(localLegacyJson, jsonCached);
-                }
-
-                LogWrapper.info("Using local " + indexName + ".json.");
-                return resourceObjects;
-            } catch (final Exception e) {
-                LogWrapper.warning("Exception loading local " + indexName + ".json: " + ExceptionUtils.getStackTrace(e));
-            } finally {
-                IOUtils.closeQuietly(fis);
-            }
-        } else {
-            LogWrapper.warning("Could not find local " + indexName + ".json.");
-        }
-
-        InputStream is = null;
+    public void handle(OutputStream os, String get, byte[] data) throws IOException {
+        JsonObject getObjects;
 
         try {
-            is = new URL(indexURL).openStream();
-            final String jsonString = IOUtils.toString(is);
-            final JsonValue json = Json.parse(jsonString);
-            final JsonObject obj = json.asObject();
-            final JsonObject resourceObjects = obj.get("objects").asObject();
-            // Cache file locally
-            FileUtils.writeStringToFile(jsonCached, jsonString);
-            LogWrapper.info("Using downloaded " + indexName + ".json.");
-            return resourceObjects;
-        } catch (final Exception e) {
-            LogWrapper.warning("Exception downloading " + indexName + ".json: " + ExceptionUtils.getStackTrace(e) + "\nThe sound fix probably won't work.");
-
-            if (e instanceof SSLHandshakeException) {
-                LogWrapper.warning("The Java installation that Minecraft is running on " +
-                                   "(" + SystemUtils.JAVA_VERSION + " (" + SystemUtils.JAVA_VENDOR + " " + SystemUtils.JAVA_VM_VERSION + ") located at " + SystemUtils.JAVA_HOME + ")" +
-                                   " may not support modern versions of TLS/SSL (e.g. TLSv1.3). Consider using a newer Java installation to fix this.");
-            }
-        } finally {
-            IOUtils.closeQuietly(is);
+            getObjects = jsonObjects.get();
+        } catch (final ConcurrentException e) {
+            LogWrapper.warning("Error lazy loading sound data: " + ExceptionUtils.getStackTrace(e));
+            return;
         }
 
-        return null;
-    }
-
-    public void handle(OutputStream os, String get, byte[] data) throws IOException {
         if (url.equals(get)) {
+            final byte[] soundsList;
+
+            switch (resourcesFormat) {
+            case CLASSIC:
+                soundsList = jsonResourcesToClassic(getObjects).getBytes();
+                break;
+
+            case AWS:
+                soundsList = jsonResourcesToAwsXml(getObjects).getBytes();
+                break;
+
+            default:
+                LogWrapper.severe("According to all known laws of Javiation, there is no way that a switch should be able to reach this case.");
+                soundsList = null;
+                break;
+            }
+
             os.write(soundsList);
         } else {
             final String name = resourcesPattern.matcher(get).replaceAll("");
-            final byte[] bytes = getResourceByName(name);
+            final byte[] bytes = getResourceByName(name, getObjects);
 
             if ((bytes != null) && (bytes.length > smallestSize)) {
                 os.write(bytes);
@@ -184,7 +129,7 @@ public final class ResourcesHandler extends EmulatorHandler {
     }
 
     // TODO @Nullable?
-    private byte[] getResourceByName(final String resParam) throws IOException {
+    private byte[] getResourceByName(final String resParam, JsonObject jsonObjects) throws IOException {
         String res = resParam;
 
         if (resourcesFormat == ResourcesFormat.CLASSIC) {
@@ -215,15 +160,13 @@ public final class ResourcesHandler extends EmulatorHandler {
         FileOutputStream resFile = null;
 
         try {
-            if (jsonObjects == null) {
-                throw new IllegalStateException("Could not download or find " + indexName + ".json!");
-            }
+            final JsonValue getRequest = jsonObjects.get(res);
 
-            if (jsonObjects.get(res) == null) {
+            if (getRequest == null) {
                 throw new IllegalStateException("No hash for resource " + res + " in " + indexName + ".json!");
             }
 
-            final String hash = jsonObjects.get(res).asObject().get("hash").asString();
+            final String hash = getRequest.asObject().get("hash").asString();
             LogWrapper.fine(res + " " + hash);
             final File localLauncherObject = FileUtil.tryFindFirstFile(
                                                  new File(Launch.minecraftHome + File.separator + "assets" + File.separator + "objects" + File.separator + hash.substring(0, 2), hash),
